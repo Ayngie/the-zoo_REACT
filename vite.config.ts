fd6b7a6ab5042834877735ad83317027da25f0c9
Ayngie <angelicareutersward@gmail.com>
2023-09-20 13:15:12 +0200

    Fixed base route
    Co-authored-by: Per Berge <bergeper@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/the-zoo_REACT/",
+  base: "/",
   build: { target: "esnext" },
   plugins: [react()],
 });
